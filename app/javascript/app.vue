eaba462e649a1a7864a2dec3dbd7de3f743cfa9d
hiro-moto-freak <hiro.nyanta.mech@gmail.com>
2022-03-30 20:59:59 +0900

    first commit

---
+++ app/javascript/app.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app id="app">
+    <div>
+      <p>{{ message }}</p>
+    </div>
+    <v-tabs>
+      <v-tab>Item One</v-tab>
+      <v-tab>Item Two</v-tab>
+      <v-tab>Item Three</v-tab>
+    </v-tabs>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Hello Vue!",
+    };
+  },
+};
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
